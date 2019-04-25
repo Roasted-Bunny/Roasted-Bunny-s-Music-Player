ba1be57955595dd5b36b97c5244f59f8b6bcacbc
Roasted Bunny <alex606@live.co.uk>
2019-04-25 18:53:11 +0100

    Added Pause Audio function and no indexing for web crawlers on other pages.

---
+++ JavaScript/Pause-Audio.js
@@ -0,0 +1,8 @@
+document.addEventListener("play", function(evt)
+{
+    if(window.$_currentlyPlaying && window.$_currentlyPlaying != evt.target)
+    {
+        window.$_currentlyPlaying.pause();
+    } 
+    window.$_currentlyPlaying = evt.target;
+}, true);
